7431afc05d4209fc1810e013d1cb85c466559683
Abdulloh-272 <167763951+Abdulloh-272@users.noreply.github.com>
2024-08-20 17:42:16 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -31,4 +31,58 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   clock();
   setInterval(clock, 1000);
+
+  const wrap = document.querySelector(".weather");
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=tashkent&appid=5f1077f561d87b1be334b15838fc01b4";
+  async function weatherFetch() {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+
+    weather(data);
+  }
+
+  weatherFetch();
+
+  function weather(item) {
+    const card = document.createElement("div");
+    const card2 = document.createElement("div");
+
+    const h3 = document.createElement("h2");
+    const h2 = document.createElement("h2");
+    const img = document.createElement("img");
+    img.classList.add("w-7");
+    card2.appendChild(img);
+    h3.textContent = Math.round(item.main.temp - 273.15) + " " + "°C";
+    h3.classList.add("text-sm");
+    h2.classList.add("text-xs");
+
+    card.append(h3, h2);
+    wrap.append(card2, card);
+    switch (item.weather[0].main) {
+      case "Clear":
+        h2.textContent = "Clear";
+        img.src = "./img/sun.png";
+        img.classList.add("animat-spin");
+        break;
+      case "Clouds":
+        h2.textContent = "Clouds";
+        img.src = "./img/cloud.png";
+        break;
+      case "Mist":
+        h2.textContent = "Mist";
+        img.src = "./img/mist.png";
+        break;
+
+      case "Rain":
+        h2.textContent = "Rain";
+        img.src = "./img/rain.png";
+        break;
+      case "Snow":
+        h2.textContent = "Snow";
+        img.src = "./img/snow.png";
+        break;
+    }
+  }
 });
